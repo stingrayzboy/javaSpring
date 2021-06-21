870406eece622ea6188de5f71dc3e683c1afce1f
Faraz Noor <faraznoor75@gmail.com>
2021-06-22 00:35:22 +0530

    Init commit

---
+++ src/main/java/com/faraz/demo/domain/Author.java
@@ -0,0 +1,58 @@
+package com.faraz.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String lastName;
+    private String firstName;
+    private Set<Book> books;
+
+    public Author(){
+
+    }
+    public Author(String lastName, String firstName, Set<Book> books) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
